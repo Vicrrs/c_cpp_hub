a9a4a4dbfe4f389fc1712d61c40c9106d7258291
Vicrrs <victorroza22@gmail.com>
2026-06-30 13:36:56 -0400

    Adiciona exemplo de tipos de variáveis em C++

---
+++ C++/005_varTypes.cpp
@@ -0,0 +1,13 @@
+/*
+int - armazenas numeros inteiros, sem decimais, como 123 ou -123
+double - armazena numeros de ponto flutuante, com casas decimais, como 19.99 ou -19.99
+char - armazena caracteres unicos, como 'a' ou 'B'.
+string - armazena teste, "Hello c++".
+bool - armazena valores com dois estados: true or false.
+
+-> Para criar uma variavel, especifique o tipo e atribua um valor a ela:
+*/
+
+int MyNum=15
+MyNum = 10
+count << MyNum;
