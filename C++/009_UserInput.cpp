2bd57d9e68687a180020017e2ec8fc368d581947
Vicrrs <victorroza22@gmail.com>
2026-06-30 13:36:56 -0400

    Adiciona exemplo de entrada do usuário em C++

---
+++ C++/009_UserInput.cpp
@@ -0,0 +1,16 @@
+/*
+cout é pronunciado "ver-out". Usado para saída e usa o operador de inserção (<<) - Em que é)
+
+cin é pronunciado "see-in". Usado para entrada e usa o operador de extração (>>) - Em que é)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cout << "Type number: ";
+	cin >> x;
+	cout << "Your number is: " << x;
+}
+
