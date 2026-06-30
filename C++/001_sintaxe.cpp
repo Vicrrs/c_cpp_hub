c8870ea7e40dd47587f1eeff8a3b542b68d31a30
Vicrrs <victorroza22@gmail.com>
2026-06-30 13:36:56 -0400

    Adiciona exemplo de sintaxe em C++

---
+++ C++/001_sintaxe.cpp
@@ -0,0 +1,8 @@
+// biblioteca de arquivos do cabecalho que permite trabalhar com objetos de entrada
+#include <iostream>// significa que podemos usar nomes para objetos e variaveis da biblioteca padrao
+// significa que podemos usar nomes para objetos e variaveis da biblioteca padrao
+using namespace std;
+int main() {
+	cout << "Olá C++" << endl;
+	return 0;
+}
