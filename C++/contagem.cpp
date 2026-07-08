e49fe5d5d8d7caba38f126114ccc6208965f7166
Vicrrs <victorroza22@gmail.com>
2026-07-07 21:00:58 -0400

    Adiciona exemplo de contagem com laço em C++

---
+++ C++/contagem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, i = 1;
+
+	cout << "Digite um numero inteiro positivo: ";
+	cin >> n;
+
+	while (i <=n) {
+		cout << i << endl;
+		i++;
+	}
+	return 0;
+}
+
