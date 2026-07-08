56ee5d9b9b94388a03bd690d79fc766b254b02a9
Vicrrs <victorroza22@gmail.com>
2026-07-07 21:01:22 -0400

    Adiciona verificação de número par em C++

---
+++ C++/verifica_par.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "Digite um numero inteiro: ";
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "O numero e par." << endl;
+	}
+	return 0;
+}
+
