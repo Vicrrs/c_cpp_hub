b836a6d039cb8b930e1156448b3adae896f436e0
Vicrrs <victorroza22@gmail.com>
2026-07-07 21:01:08 -0400

    Adiciona exemplo de operadores relacionais em C++

---
+++ C++/relational.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+	int x;
+
+	cout << "Enter a number: " << endl;
+	cin >> x;
+
+	printf("%d > 10 -> %d\n", x, x > 10);
+
+
+}
+
