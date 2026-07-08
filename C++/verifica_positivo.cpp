89b2b9162b6ff6793807a07cdb4a4def0e6d5acd
Vicrrs <victorroza22@gmail.com>
2026-07-07 21:01:24 -0400

    Adiciona verificação de número positivo em C++

---
+++ C++/verifica_positivo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double x;
+	
+	cout << "Digite um numero: ";
+	cin >> x;
+
+	if (x > 0) {
+		cout << "O numero é positivo" << endl;
+	}
+	
+	return 0;
+}
+
